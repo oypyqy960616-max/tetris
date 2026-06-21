27d3b3eb2bfd4c00e656b3633007a08e9211d460
hu <oypyqy960616@gmail.com>
2026-06-21 16:52:52 +0800

    new file:   1x1.png 	modified:   main.lua

--- main.lua
+++ main.lua
@@ -1,96 +1,171 @@
-boardWidth = 10--地圖寬度(欄)
-boardHeight = 20--地圖高度(列)
+--全域設定
+tileSize = 64   --小方格大小
+boardWidth = 10 --地圖寬
+boardHeight = 9 --地圖高
+dropTimer = 0   --目前累積掉落時間
+dropDelay = 1   ----每1秒掉一格
 
-board = {}--建立空白地圖
+--方塊座標
+shapes = {
+    L = {
+        { x = 0, y = 0 },
+        { x = 0, y = 1 },
+        { x = 0, y = 2 },
+        { x = 1, y = 2 },
+    },
+    T = {
+        { x = 0, y = 0 },
+        { x = 1, y = 0 },
+        { x = 2, y = 0 },
+        { x = 1, y = 1 }
+    },
+    I = {
+        { x = 0, y = 0 },
+        { x = 1, y = 0 },
+        { x = 2, y = 0 },
+        { x = 3, y = 0 }
+    },
+    O = {
+        { x = 0, y = 0 },
+        { x = 1, y = 0 },
+        { x = 0, y = 1 },
+        { x = 1, y = 1 }
+    }
+}
 
-for y = 1, boardHeight do--延伸y列(從第1列建立到第20列)
-    board[y] = {}--建立第y列
+--建立地圖表格
+board = {}                   --建立空白地圖
+
+for y = 1, boardHeight do    --延伸y列(從第1列建立到第20列)
+    board[y] = {}            --建立第y列
     for x = 1, boardWidth do --在y上進行x的延伸(在這一列建立第1欄到第10欄)
-        board[y][x] = 0--建立空格
+        board[y][x] = 0      --建立空格
     end
 end
 
-L={
-{x=0,y=0},
-{x=0,y=1},
-{x=0,y=2},
-{x=1,y=2}
-}
-T={
-{x=0,y=0},
-{x=1,y=0},
-{x=2,y=0},
-{x=1,y=1}
-}
---J={
---{x=2,y=0},
---{x=0,y=1},
---{x=1,y=1},
---{x=2,y=1}
---}
-I={
-{x=0,y=0},
-{x=1,y=0},
-{x=2,y=0},
-{x=3,y=0}
-} 
-O={
-{x=0,y=0},
-{x=1,y=0},
-{x=0,y=1},
-{x=1,y=1}
-}
-
-function love.load()
-
-end
-
+--行動
 function love.keypressed(key)
-  
+    if key == "a" then
+        if canMove(currentShape, pieceX - 1, pieceY) then
+            pieceX = pieceX - 1
+        end
+    elseif key == "d" then
+        if canMove(currentShape, pieceX + 1, pieceY) then
+            pieceX = pieceX + 1
+        end
+    elseif key == "space" then
+        if canMove(currentShape, pieceX, pieceY + 1) then
+            pieceY = pieceY + 1
+        end
+    end
 end
 
-function love.update(dt)
+--碰撞判斷
+function canMove(shape, posX, posY)     --建立canMove功能用於確認方塊是否可以移動新的位置
+    for _, block in ipairs(shape) do    --輪詢 shape 每一個組成方塊(block)
+        local x = posX + block.x        -- 計算方塊預期移動後的 x 位置(計算此小方塊移動後的實際 X 座標
+        local y = posY + block.y        -- 計算方塊預期移動後的 y 位置
 
-end
-
-function love.draw()
-
-end
-
-local function canMove(shape, posX, posY)--建立canMove功能用於確認方塊是否可以移動新的位置
-    for index, block in ipairs(shape) do --輪詢 shape 每一個組成方塊(block)
-        local x = posX + block.x -- 計算方塊預期移動後的 x 位置(計算此小方塊移動後的實際 X 座標
-        local y = posY + block.y -- 計算方塊預期移動後的 y 位置
-
-        if x < 1 or x > boardWidth then--判斷x是否超出地圖左右邊界
+        if x < 1 or x > boardWidth then --判斷x是否超出地圖左右邊界
             return false
         end
 
-        if y > boardHeight then--判斷y是否超出地圖底部
+        if y > boardHeight then --判斷y是否超出地圖底部
             return false
         end
 
-        if board[y] and board[y][x] == 1 then--判斷地圖中的x y 座標是否已有固定方塊
+        if board[y] and board[y][x] == 1 then --判斷地圖中的x y 座標是否已有固定方塊
             return false
         end
     end
 
     return true
 end
-function spawnPiece()--建立新的隨機方塊
 
-    local types = {--可生成的方塊種類
+--方塊固定
+function lockPiece()                        --建立固定方塊功能
+    for _, block in ipairs(currentShape) do --一個取出目前形狀每個小格的循環
+        local x = pieceX + block.x          --目前方塊位置 + 小方塊偏移量 = 實際x座標
+        local y = pieceY + block.y          --目前方塊位置 + 小方塊偏移量 = 實際y座標
+
+        board[y][x] = 1                     --將x y標記方格占用
+    end
+end
+
+--新方塊生成
+function spawnPiece() --建立新的隨機方塊
+    local types = {   --可生成的方塊種類
         shapes.L,
         shapes.T,
-        shapes.J,
         shapes.I,
         shapes.O
     }
 
-    currentShape = types[math.random(#types)]-- 隨機取得一個方塊形狀#types/ = types內元素數量/math.random() = 隨機產生編號/types[編號] = 取出對應方塊
+    currentShape = types[math.random(#types)] -- 隨機取得一個方塊形狀#types/ = types內元素數量/math.random() = 隨機產生編號/types[編號] = 取出對應方塊
 
 
-    pieceX = 5--新方塊生成的X位置
-    pieceY = 1--新方塊生成的Y位置
+    pieceX = 5 --新方塊生成的X位置
+    pieceY = 1 --新方塊生成的Y位置
+end
 
+sidePanelWidth = 200
+windowWidth = boardWidth * tileSize + sidePanelWidth
+windowHeight = boardHeight * tileSize
+
+function love.load()
+    love.window.setMode(windowWidth, windowHeight)
+
+    blockImage = love.graphics.newImage("1x1.png")
+    spawnPiece()
+end
+
+--畫出指定方格
+function drawPiece(shape, posX, posY)                 --畫圖(指定圖案)
+    for _, block in ipairs(shape) do                  --依序取出形狀中的每個小方塊
+        local drawX = (posX + block.x - 1) * tileSize --要畫出方塊的x位子=(方塊的x+小方格的x-1)*它的大小  將格子座標轉換成螢幕像素座標/ -1是因為格子從1開始，而像素從0開始
+
+        local drawY = (posY + block.y - 1) * tileSize --同上
+
+        love.graphics.draw(blockImage, drawX, drawY)  --將 blockImage 畫到(drawX, drawY)
+    end
+end
+
+function love.load()                               --初始化(圖 分數-0 音樂 第一個方塊都屬於)
+    blockImage = love.graphics.newImage("1x1.png") --載入方塊圖片
+    spawnPiece()                                   --生成第一個方塊
+end
+
+--時間流動
+function love.update(dt)                                  --每幀進行一次
+    dropTimer = dropTimer + dt                            --目前累積掉落時間=累積時間+變化時間
+    if dropTimer >= dropDelay then                        --如果時間累積到1秒
+        dropTimer = 0                                     --計時器歸零
+        if canMove(currentShape, pieceX, pieceY + 1) then --如果目前方塊可向下移動一格
+            pieceY = pieceY + 1                           --目前方塊往y軸+1格
+        else                                              --如果無法移動
+            lockPiece()                                   --固定目前方塊到地圖
+            spawnPiece()                                  --生成新方塊
+        end
+    end
+end
+
+--畫出地圖
+function drawBoard()
+    for y = 1, boardHeight do           --檢查y列
+        for x = 1, boardWidth do        --檢查x欄
+            if board[y][x] == 1 then    --判斷這格是否存在方塊
+                love.graphics.draw(     --畫圖
+                    blockImage,         --我的1x1.png
+                    (x - 1) * tileSize, --計算x
+                    (y - 1) * tileSize  --計算y
+                )
+            end
+        end
+    end
+end
+
+--畫圖功能
+function love.draw()                        --每幀呼叫一次，負責繪製畫面
+    drawBoard()                             --畫出固定方塊
+    drawPiece(currentShape, pieceX, pieceY) --將目前方塊畫到畫面上
 end
